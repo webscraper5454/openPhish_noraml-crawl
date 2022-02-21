fe40022d520bb5bb5cab9a2afb73d18ec528be33
its_sam <98635210+webscraper5454@users.noreply.github.com>
2022-02-21 15:04:17 +0530

    Advanced openPhis data collection

--- openPhis.py
+++ openPhis.py
@@ -1,7 +1,7 @@
 from curses.ascii import isspace
 import requests
 from bs4 import BeautifulSoup
-
+import os
 
 file_write = open('openPhis_domains.txt','w')
 url = requests.get('https://openphish.com/').content
@@ -16,4 +16,17 @@ for i in  data:
         if not isspace(j):
             data = j.text
             file_write.write(data)
+
+
+url2 = requests.get("https://openphish.com/feed.txt")
+data = url2.content
+
+soup2 = BeautifulSoup(data,"html.parser")
+soup2.find("body")
+file_write.write(str(soup2))
+
+   
+
+
+
             
